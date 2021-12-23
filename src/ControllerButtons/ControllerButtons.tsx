c3ed3c9dfd6561ce00fb36e789917fd0cd2413a2
quietsato <quietsato@gmail.com>
2021-12-23 15:08:02 +0900

    Apply simple stylesheet

--- src/ControllerButtons/ControllerButtons.tsx
+++ src/ControllerButtons/ControllerButtons.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { ControllerProps as CanvasControllerProps } from "../HandwriteCanvas/HandwriteCanvas";
 
+import "./ControllerButtons.css"
+
 export type Props = CanvasControllerProps & React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
 const ControllerButtons: React.FC<Props> = (props) => {
@@ -11,8 +13,24 @@ const ControllerButtons: React.FC<Props> = (props) => {
     } = props;
     return (
         <div {...rootProps}>
-            <button onClick={onImageDataRequested}>Predict</button>
-            <button onClick={onClearCanvas}>Clear Canvas</button>
+            <button
+                className={[
+                    "ControllerButtons__Button",
+                    "ControllerButtons__PredictButton"
+                ].join(" ")}
+                onClick={onImageDataRequested}
+            >
+                Predict
+            </button>
+            <button
+                className={[
+                    "ControllerButtons__Button",
+                    "ControllerButtons__ClearButton"
+                ].join(" ")}
+                onClick={onClearCanvas}
+            >
+                Clear Canvas
+            </button>
         </div>
     )
 };
