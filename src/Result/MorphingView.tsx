3b118cce8500b5a45549dc95abbfcc2ef0b14f47
quietsato <quietsato@gmail.com>
2021-12-24 15:04:35 +0900

    Morphing View

---
+++ src/Result/MorphingView.tsx
@@ -0,0 +1,30 @@
+
+import React from "react";
+import { imageDataToDataUrl } from "../ImageConverter/ImageConverter";
+
+import "./MorphingView.css";
+
+type Props = {
+  morphingImages: ImageData[] | null
+};
+
+const MorphingView: React.FC<Props> = (props) => {
+  if (!props.morphingImages) {
+    return <></>
+  }
+  return (
+    <div className="MorphingView">
+      {props.morphingImages.map((image, i) => (
+        <div key={i} className="MorphingView__Item">
+          <img
+            className="MorphingView__Item__Image"
+            src={imageDataToDataUrl(image)}
+            alt={`Morphing Image (${i + 1} of ${props.morphingImages!!.length})`}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default MorphingView;
